e4ede7d27fc6e9aff7b61511109a0e4d8a5e86c1
Christof Salis <c.salis@vertifymed.com>
2025-02-23 15:23:44 +0100

    feat: add support for 'tabs' mention in context menus and diagnostics

--- webview-ui/src/components/chat/ChatTextArea.tsx
+++ webview-ui/src/components/chat/ChatTextArea.tsx
@@ -432,6 +432,8 @@ const ChatTextArea = forwardRef<HTMLTextAreaElement, ChatTextAreaProps>(
 						insertValue = "problems"
 					} else if (type === ContextMenuOptionType.Terminal) {
 						insertValue = "terminal"
+					} else if (type === ContextMenuOptionType.Tabs) {
+						insertValue = "tabs"
 					} else if (type === ContextMenuOptionType.Git) {
 						insertValue = value || ""
 					}
